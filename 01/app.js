d3f0a14d6b1414bba26eebdce98a18f9b27c237f
Katarzyna Dworak <k.dw@poczta.onet.pl>
2024-06-17 11:08:01 +0200

    01 done

--- 01/app.js
+++ 01/app.js
@@ -1,3 +1,4 @@
+//ok 
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
@@ -7,10 +8,30 @@ class App extends React.Component {
     state = {
         counter: 0,
     }
-    render() {
-        console.log('render');
+    constructor (props) {
+        super(props);
+        this.state = { counter: 0 };
+        console.log('constructor');
+    }
 
-        return <h1>{ this.state.counter }</h1>
+    componentDidMount() {
+        console.log('componentDidMount')
+        this.id = setInterval(() => {
+            this.setState((prevState) => ({ counter: prevState.counter + 1 }));
+            console.log('Interval running');  
+        }, 5000);
+    }
+
+    componentDidUpdate() {
+            console.log('componentDidUpdate');
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.id);
+    }
+
+    render() {
+        return <h1>{ this.state.counter }</h1>;
     }
 }
 
